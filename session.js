f5bc9afa3413aec0dacb3788bed33a4fbc04b431
Chamidumilam387 <Chamidumilan387@gmail.com>
2025-04-04 08:57:46 +0530

    Update session.js

--- session.js
+++ session.js
@@ -41,7 +41,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~aIt0xLTR#1KVjqp9EPzMdQfn-09Ux62uJ48Jhm5wotLp_B5ov59o",
-SUDO: process.env.SUDO || ["94717775628","94758775628"],
+SUDO: process.env.SUDO || ["94762862143","94758775628"],
 PORT: process.env.PORT || "8000",
-FOOTER: process.env.FOOTER || "> QUEEN ANJU XPRO\n> Developed by Mr. Rashmika\n> GitHub Repository: github.com/Mrrashmika/QUEEN_ANJU_xPro\n🌟 Premium Version | Advanced Features | Secure & Reliable 🌟\n© 2025 QUEEN ANJU XPRO. All rights reserved.",
+FOOTER: process.env.FOOTER || "> 𝙵𝙸𝙻𝙼 𝚆𝙾𝚁𝙻𝙳 | 𝙰𝙳𝙼𝙸𝙽 𝙱𝚈 𝚃𝙷𝙰𝚁𝚄𝚂𝙷𝙰.",
 };
