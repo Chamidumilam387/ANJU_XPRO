dcc07fcaa2bcc309187f5f88ad9032c2e8b13078
Chamidumilam387 <Chamidumilan387@gmail.com>
2025-04-10 20:52:30 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,12 +40,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "your session id",
-    GITHUB_TOKEN: process.env.GITHUB_TOKEN || "github token without ghp_",
-    GITHUB_USERNAME: process.env.GITHUB_USERNAME || "github username",
-    SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628", "94758775628"],
+    SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~aIt0xLTR#1KVjqp9EPzMdQfn-09Ux62uJ48Jhm5wotLp_B5ov59o",
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN || "BSBnXUNb5X30rkVYluXEgir979dEe73FDsJO",
+    GITHUB_USERNAME: process.env.GITHUB_USERNAME || "Chamidumilam387",
+    SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94762862143", "94758775628"],
     PORT: process.env.PORT || "8000",
     SESSION_DIR: process.env.SESSION_DIR || "session",
-    FOOTER: process.env.FOOTER || "> QUEEN ANJU XPRO\n> Developed by Mr. Rashmika\n> GitHub Repository: github.com/Mrrashmika/QUEEN_ANJU_xPro\n\ud83c\udf1f Premium Version | Advanced Features | Secure & Reliable \ud83c\udf1f\n\u00a9 2025 QUEEN ANJU XPRO. All rights reserved." 
+    FOOTER: process.env.FOOTER || "> 𝙵𝙸𝙻𝙼 𝚆𝙾𝚁𝙻𝙳 | 𝙰𝙳𝙼𝙸𝙽 𝚃𝙷𝙰𝚁𝚄𝚂𝙷𝙰 ©." 
   };
   
